7054032835127f8de9c18378ed2bc0e3bd2c7836
samirvithlani <samir.vithlani83955@gmail.com>
2023-12-31 15:08:01 +0530

    join

---
+++ src/com/thread/ThreadDemo5.java
@@ -0,0 +1,46 @@
+package com.thread;
+
+public class ThreadDemo5 extends Thread {
+
+	@Override
+	public void run() {
+
+		for (int i = 1; i <= 5; i++) {
+
+			System.out.println(i + "  Thread is running..." + Thread.currentThread().getName());
+		}
+	}
+
+	public static void main(String[] args) {
+
+		ThreadDemo5 t1 = new ThreadDemo5();
+		ThreadDemo5 t2 = new ThreadDemo5();
+		ThreadDemo5 t3 = new ThreadDemo5();
+
+		t1.setName("RED");
+		t2.setName("GREEN");
+		t3.setName("YELLOW");
+		
+		
+		t1.start();
+		try {
+			t1.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		t2.start();
+		try {
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		t3.start();
+		
+		
+		
+		
+
+	}
+}
