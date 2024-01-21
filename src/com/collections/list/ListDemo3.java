e11b9e11b54b2f509d0dc18ec0e4332146283d1e
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-21 15:25:48 +0530

    changes

---
+++ src/com/collections/list/ListDemo3.java
@@ -0,0 +1,44 @@
+package com.collections.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Student{
+	
+	int sId;
+	String sName;
+	public Student(int sId, String sName) {
+		
+		this.sId = sId;
+		this.sName = sName;
+	}
+	
+	
+	
+}
+public class ListDemo3 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		List<Student> students  = new ArrayList<Student>();
+		Student s1 = new Student(101, "ram");
+		Student s2 = new Student(102, "seeta");
+		
+		students.add(s1);
+		students.add(s2);
+		students.add(new Student(103, "lakshman"));
+		
+		
+		for(Student s:students) {
+			
+			System.out.println(s.sId + " " +s.sName);
+		}
+		
+		
+		
+		
+		
+	}
+}
